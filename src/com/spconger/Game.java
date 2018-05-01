9b66c4de97c067be0630cddf38158e8a6c8f0fea
Steve Conger <spconger@gmail.com>
2018-05-01 09:50:13 -0700

    Card Game commit

---
+++ src/com/spconger/Game.java
@@ -0,0 +1,53 @@
+package com.spconger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public abstract class Game implements IGame{
+	/*
+	 * This as an abstract class from which
+	 * each card game type will inherits
+	 * It also implements the interface
+	 * IGame. Because this class is abstract
+	 * it does not have to implement
+	 * the interface methods, but all
+	 * it's children do.
+	 */
+	
+	//every game will have a deck
+    protected Deck deck;
+    //every game should have a description
+    private String gameDescription;
+    
+    //sets and gets for the fields
+	public Deck getDeck() {
+		return deck;
+	}
+	
+	public void setDeck(Deck deck) {
+		this.deck = deck;
+	}
+	
+	
+	public void shuffleDeck() {
+		Deck d = getDeck();
+		ArrayList<Card> cards = d.getDeck();
+		Collections.shuffle(cards);
+		d.setDeck(cards);
+		
+	}
+	
+	public String getGameDescription() {
+		return gameDescription;
+	}
+	public void setGameDescription(String gameDescription) {
+		this.gameDescription = gameDescription;
+	}
+	public String toString(){
+		return "Put name of the game here";
+	}
+	
+	//shuffles the deck
+
+	
+}
